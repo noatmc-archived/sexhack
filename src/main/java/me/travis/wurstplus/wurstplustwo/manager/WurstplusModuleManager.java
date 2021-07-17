28154be17de2b8c87f9f2f9f4dc656b02d30bc6d
noat <noatprotonvpn@gmail.com>
2021-07-17 19:21:54 +0700

    rena would be proud of me

--- src/main/java/me/travis/wurstplus/wurstplustwo/manager/WurstplusModuleManager.java
+++ src/main/java/me/travis/wurstplus/wurstplustwo/manager/WurstplusModuleManager.java
@@ -71,6 +71,7 @@ public class WurstplusModuleManager {
 		add_hack(new WurstplusPortalGodMode());
 		add_hack(new WurstplusPacketMine());
 		add_hack(new WurstplusBurrow());
+		add_hack(new WurstplusGodModule());
 		add_hack(new WurstplusEntityMine());
 		add_hack(new WurstplusBuildHeight());
 		add_hack(new WurstplusCoordExploit());
